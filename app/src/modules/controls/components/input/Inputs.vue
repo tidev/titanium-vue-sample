0230d4a10f4fedba6983edf09cedd6ce0910f976
Jan Vennemann <jan.vennemann@gmx.net>
2018-08-02 23:23:30 +0200

    add button demo

--- app/src/modules/controls/components/input/Inputs.vue
+++ app/src/modules/controls/components/input/Inputs.vue
@@ -1,24 +1,29 @@
 <template>
-  <base-window title="Input Elements" layout="vertical">
-    <input-demo ref="switchDemo" name="Switch" hint="Tap the switch">
-      <switch v-model="switchValue" right="20" ios:onTintColor="#4fc08d"/>
-    </input-demo>
+  <base-window title="Input Elements">
+    <scroll-view layout="vertical">
+      <input-demo ref="switchDemo" name="Switch" hint="Tap the switch">
+        <switch v-model="switchValue" right="20" ios:onTintColor="#4fc08d"/>
+      </input-demo>
 
-    <input-demo ref="sliderDemo" name="Slider" hint="Move the slider">
-      <slider v-model="sliderValue" min="0" max="100" ios:tintColor="#4fc08d"/>
-    </input-demo>
+      <input-demo ref="sliderDemo" name="Slider" hint="Move the slider">
+        <slider v-model="sliderValue" min="0" max="100" ios:tintColor="#4fc08d"/>
+      </input-demo>
 
-    <input-demo ref="textFieldDemo" name="Text Field">
-      <text-field v-model="textFieldValue" left="0" hintText="Enter some text"/>
-    </input-demo>
+      <input-demo ref="buttonDemo" name="Button" hint="Tap the button">
+        <base-button @click="onSubmit">Submit</base-button>
+      </input-demo>
 
-    <input-demo ref="textAreaDemo" name="Text Area" hint="Enter some text">
-      <vertical-layout>
-        <text-area left="0" right="0" height="50" v-model="textAreaValue" hintText="Enter some text">
-        </text-area>
-        <label height="50" backgroundColor="red">{{textAreaValue}}</label>
-      </vertical-layout>
-    </input-demo>
+      <input-demo ref="textFieldDemo" name="Text Field">
+        <text-field v-model="textFieldValue" left="0" hintText="Enter some text"/>
+      </input-demo>
+
+      <input-demo ref="textAreaDemo" name="Text Area" hint="Enter some text">
+        <vertical-layout :height="Ti.UI.SIZE">
+          <text-area left="0" right="0" height="50" v-model="textAreaValue" hintText="Enter some text"></text-area>
+          <label height="50" backgroundColor="red">{{textAreaValue}}</label>
+        </vertical-layout>
+      </input-demo>
+    </scroll-view>
   </base-window>
 </template>
 
@@ -47,6 +52,11 @@ export default {
     },
     textFieldValue(newVal) {
       this.$refs.textFieldDemo.state = newVal !== '' ? `Text: ${newVal}` : '';
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.buttonDemo.state = 'Nicely done!'
     }
   }
 }
